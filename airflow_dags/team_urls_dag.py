6fc64326d9258990e9e827f21f6b018acf3051d4
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-11-23 16:35:37 +0100

    Team URLs DAG

---
+++ airflow_dags/team_urls_dag.py
@@ -0,0 +1,10 @@
+# import datetime
+# #import logging
+#
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+#
+# dag = DAG(
+#     'team_urls_dag',
+#     schedule_interval='@hourly',
+#     start_date=datetime.datetime.now() - datetime.timedelta(days=1))
