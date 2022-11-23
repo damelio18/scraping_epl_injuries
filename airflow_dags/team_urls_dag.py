317d7567b1a4b98730d8cb6c91f7fa0ae2b0feff
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-11-23 16:46:36 +0100

    Team URLs DAG

--- airflow_dags/team_urls_dag.py
+++ airflow_dags/team_urls_dag.py
@@ -1,10 +1,23 @@
-# import datetime
-# #import logging
-#
-# from airflow import DAG
-# from airflow.operators.python_operator import PythonOperator
-#
-# dag = DAG(
-#     'team_urls_dag',
-#     schedule_interval='@hourly',
-#     start_date=datetime.datetime.now() - datetime.timedelta(days=1))
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def greet():
+	logging.info('Hello World Engineers!'
+
+
+dag = DAG(
+    'team_urls_dag',
+    schedule_interval='@hourly',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1))
+
+greet_task = PythonOperator(
+    task_id="greet_task",
+    python_callable=greet,
+    dag=dag
+)
+
+
+greet_task
