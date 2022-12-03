fa2766fc5cf413f122bbf33e1e2cc2fe07af24a9
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-03 11:56:42 +0100

    DAGs

---
+++ airflow_dags/scrape_team_urls_DAG.py
@@ -0,0 +1,23 @@
+import datetime
+#import logging
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+def team_urls:
+    print("Hello")
+
+
+dag = DAG(
+    'scrape_team_urls_DAG',
+    schedule_interval='@hourly',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1))
+
+team_urls_task = PythonOperator(
+    task_id="team_urls_task",
+    python_callable=team_urls,
+    dag = dag
+)
+
+team_urls_task
