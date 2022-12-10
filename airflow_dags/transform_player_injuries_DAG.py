09edfee3c04859db4d3c22ba4bd04cfdc0109511
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-10 12:17:18 +0100

    DAGs

--- airflow_dags/transform_player_injuries_DAG.py
+++ airflow_dags/transform_player_injuries_DAG.py
@@ -62,7 +62,7 @@ def get_injuries():
     injuries_df_2 = injuries_df_2.replace(['NA'], np.nan)
     injuries_df_2['date_until'] = injuries_df_2['date_until'].replace(['-'], np.nan)
     injuries_df_2['games_missed'] = injuries_df_2['games_missed'].replace(['?', '-'], "0").astype('float')
-    #injuries_df_2[['int_caps', 'int_goals']] = injuries_df_2[['int_caps', 'int_goals']].fillna('0')
+    injuries_df_2[['int_caps', 'int_goals']] = injuries_df_2[['int_caps', 'int_goals']].fillna('0')
 
     # Revert DataFrame to list
     injuries_df_2 = injuries_df_2.values.tolist()
