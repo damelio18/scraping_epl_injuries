ec124c494e03f81bafb2602ded2eb9f84fcc4b8c
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-10 09:59:08 +0100

    DAGs

--- airflow_dags/transform_player_injuries_DAG.py
+++ airflow_dags/transform_player_injuries_DAG.py
@@ -39,7 +39,7 @@ def load_injuries():
     print("111")
 
     # Fetch all data from table
-    tuples_list = cursor.fetchall()
+    tuples_list = str(cursor.fetchall())
     #df = pd.read_sql(cursor)
 
     print("222")
