9e006848821e5c07b950dbfdf2350bfb48574e2d
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-14 18:21:14 +0100

    DAGs

--- airflow_dags/create_tables_DAG.py
+++ airflow_dags/create_tables_DAG.py
@@ -89,7 +89,7 @@ def bios():
     # Insert the rows into the database
     pg_hook_2.insert_rows(table="store_player_bios", rows=rows)
 
-    return rows
+    return tuples_list
 
 
 # 4. Log the end of the DAG
