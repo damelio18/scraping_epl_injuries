3cacf3dd18d82f266c14810fa3d7716d14e61d8a
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-14 15:39:57 +0100

    DAGs

--- airflow_dags/transform_player_injuries_DAG.py
+++ airflow_dags/transform_player_injuries_DAG.py
@@ -40,7 +40,7 @@ def stg_table():
     cursor_1.execute(sql_statement_get_data)
     tuples_list = cursor_1.fetchall()
 
-    tuples_list = tuples_list[40:51]
+    tuples_list = tuples_list[:51]
 
     # ----------------------------- Create Staging Table in Data Warehouse -----------------------------
     # Data warehouse credentials
