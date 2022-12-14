037e3dc40f68ce488b02ce2865a82cb8225bfdcd
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-14 15:56:02 +0100

    DAGs

--- airflow_dags/transform_player_injuries_DAG.py
+++ airflow_dags/transform_player_injuries_DAG.py
@@ -226,8 +226,8 @@ def date_columns():
     # Create Age column: Convert age to years
     df['age'] = round(df['age'] / np.timedelta64(1, 'Y'), 0)
     #
-    # # Clean date_from column
-    # clean_date(df, 'date_from')
+    # Clean date_from column
+    clean_date(df, 'date_from')
     #
     # # Clean date_until column
     # clean_date(df, 'date_until')
