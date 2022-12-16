84c98ea1914d3d922a0c056e9bcf36c0e2e78670
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-16 21:49:41 +0100

    DAGs

--- airflow_dags/5_create_tables_DAG.py
+++ airflow_dags/5_create_tables_DAG.py
@@ -268,11 +268,11 @@ def injuries():
     sql_drop_table = "DROP TABLE IF EXISTS store_historical_injuries"
 
     # SQL Statement: Create new table
-    sql_create_table = "CREATE TABLE IF NOT EXISTS store_historical_injuries (code VARCHAR(255)," \
-                       " season VARCHAR(255), injury VARCHAR(255), date_from_day VARCHAR(255)," \
-                       "date_from_mon VARCHAR(255), date_from_year VARCHAR(255), date_from VARCHAR(255)," \
-                       "date_until_day VARCHAR(255), date_until_mon VARCHAR(255), date_until_year VARCHAR(255)," \
-                       "date_until VARCHAR(255), days_injured VARCHAR(255), games_missed VARCHAR(255));"
+    sql_create_table = "CREATE TABLE IF NOT EXISTS store_historical_injuries (code int," \
+                       "season VARCHAR(255), injury VARCHAR(255), date_from_day int," \
+                       "date_from_mon int, date_from_year int, date_from VARCHAR(255)," \
+                       "date_until_day int, date_until_mon int, date_until_year int," \
+                       "date_until VARCHAR(255), days_injured int, games_missed int);"
 
     # Drop and create table
     cursor_2.execute(sql_drop_table)
