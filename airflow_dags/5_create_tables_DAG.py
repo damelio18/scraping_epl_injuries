31d8e953bd3ba794999fdc28fff058b743badb60
Daniel D'Amelio <danny.g.damelio@gmail.com>
2022-12-17 11:34:09 +0100

    DAGs

--- airflow_dags/5_create_tables_DAG.py
+++ airflow_dags/5_create_tables_DAG.py
@@ -40,7 +40,7 @@ def assign_ids():
     cursor_1.execute(sql_statement_get_data)
     tuples_list_1 = cursor_1.fetchall()
 
-    tuples_list_1 = tuples_list_1[:30]
+    #tuples_list_1 = tuples_list_1[:30]
 
     # Create DataFrame
     column_names = ['dob', 'height', 'nationality', 'int_caps', 'int_goals',
@@ -203,7 +203,7 @@ def bios():
     sql_drop_table = "DROP TABLE IF EXISTS store_player_bios"
 
     # SQL Statement: Create new table
-    sql_create_table = "CREATE TABLE IF NOT EXISTS store_player_bios (code int PRIMARY KEY," \
+    sql_create_table = "CREATE TABLE IF NOT EXISTS store_player_bios (code int," \
                        "first_name VARCHAR(255), second_name VARCHAR(255), current_club VARCHAR(255)," \
                        "dob_day int, dob_mon int, dob_year int," \
                        "dob date, age int, height int," \
