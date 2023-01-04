b4fea7efa86c601915b5c072e66864f58c373d14
Daniel D'Amelio <danny.g.damelio@gmail.com>
2023-01-04 11:33:04 +0100

    DAGs

--- airflow_dags/5_create_tables_DAG.py
+++ airflow_dags/5_create_tables_DAG.py
@@ -183,8 +183,9 @@ def bios():
     ################ Create injury KPI
 
     # Change columns to numeric type
-    #df['games_missed'] = df['games_missed'].replace(['NaN'], 0.0)
-    #df = df.replace(['NaN'], np.nan)
+    df['games_missed'] = df['games_missed'].replace(['NaN'], 0.0)
+    df = df.fillna("")
+
     change_type = ['age', 'games_missed']
     df[change_type] = df[change_type].apply(pd.to_numeric)
 
