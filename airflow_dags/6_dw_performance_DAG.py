6e1430203ae5c70988e657ad9925eee32519462e
Daniel D'Amelio <danny.g.damelio@gmail.com>
2023-01-05 14:40:03 +0100

    DAGs

--- airflow_dags/6_dw_performance_DAG.py
+++ airflow_dags/6_dw_performance_DAG.py
@@ -44,7 +44,7 @@ def join_data():
                     'dob_day', 'dob_mon', 'dob_year', 'dob', 'age', 'height',
                     'nationality', 'int_caps', 'int_goals', 'injury_risk']
 
-    df1 = pd.DataFrame(tuples_list_1, columns = column_names)
+    #df1 = pd.DataFrame(tuples_list_1, columns = column_names)
 
     return tuples_list_1
 
