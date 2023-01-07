6f8e4bdb4aafde34089927428625e86539f1532a
Daniel D'Amelio <danny.g.damelio@gmail.com>
2023-01-07 19:07:34 +0100

    DAGs

--- airflow_dags/6_dw_performance_DAG.py
+++ airflow_dags/6_dw_performance_DAG.py
@@ -286,7 +286,7 @@ def create_dims(ti):
     # players2 = pd.merge(players, df2,
     #                     on=['player_id'], how='left')
 
-    players.merge(df2, on='player_id')
+    players = players.merge(df2, on='player_id')
 
     # SQL Statements
     sql_create_table = "CREATE TABLE IF NOT EXISTS dim_players (player_id int PRIMARY KEY, " \
